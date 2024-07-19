976048349d574839ff545862e6ee9de59df99d9c
Dilina Raveen <dilinaraveen@gmail.com>
2024-07-19 20:27:21 +0530

    initial changes

---
+++ src/main/java/com/dilinaraveen/rent_a_car/dtos/CarDtoListDto.java
@@ -0,0 +1,11 @@
+package com.dilinaraveen.rent_a_car.dtos;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CarDtoListDto {
+
+    private List<CarDto> carDtoList;
+}
---
+++ src/main/java/com/dilinaraveen/rent_a_car/dtos/SearchCarDto.java
@@ -0,0 +1,15 @@
+package com.dilinaraveen.rent_a_car.dtos;
+
+import lombok.Data;
+
+@Data
+public class SearchCarDto {
+
+    private String brand;
+
+    private String type;
+
+    private String transmission;
+
+    private String color;
+}
